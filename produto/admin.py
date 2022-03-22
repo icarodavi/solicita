79920cd4131f1d4da2e969f27de357726927725f
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-03-21 21:21:10 -0300

    Model, Admin e Migrations Produtos e Variação

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Produto, Variacao
 
 # Register your models here.
+
+
+class VariacaoInLine(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'get_preco_format',
+                    'get_preco_promocional_format', 'tipo')
+    inlines = [VariacaoInLine]
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)
--- solicita/settings.py
+++ solicita/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'secretaria',
     'solicitacao',
     'perfil',
+    'produto',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
