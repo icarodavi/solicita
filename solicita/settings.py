0946269b4b1f6bc5d67c87d4735b4f5487dd2475
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-03-22 17:43:35 -0300

    Alteração config .env

--- solicita/settings.py
+++ solicita/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from django.contrib.messages import constants
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!rpr$sqf#gprdomso3*cyth%d+q09ede!yoiqui5t0cui6gfav'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -84,11 +85,11 @@ WSGI_APPLICATION = 'solicita.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'solicita',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': 'icaro',
-        'PORT': '3306'
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'PORT': config('DB_PORT')
     }
 }
 
