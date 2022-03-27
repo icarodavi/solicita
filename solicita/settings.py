8881293448041ddbb191c40b4a77fd89e6e2f64d
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-03-27 20:08:52 -0300

    Amazon S3

--- solicita/settings.py
+++ solicita/settings.py
@@ -163,6 +163,13 @@ django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 if DEBUG:
     def show_toolbar(request):
         return True
