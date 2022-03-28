e6081817a3a10e4626592ae299cf9b4151660189
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-03-27 23:29:24 -0300

    Update storage_backends.py

--- solicita/storage_backends.py
+++ solicita/storage_backends.py
@@ -1,6 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from decouple import config
 
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
+    bucket_name = config('AWS_STORAGE_BUCKET_NAME')
